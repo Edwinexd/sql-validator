6236f5666f455fa1df70a616858fe60fbcd5fb19
Edwin <60476129+Edwinexd@users.noreply.github.com>
2025-10-14 17:14:11 +0200

    Declare environmental variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -19,3 +19,10 @@ declare module "@djthoms/pretty-checkbox" {
   const content: unknown;
   export default content;
 }
+
+declare interface ImportMetaEnv {
+  readonly VITE_PRIVACY_CF_WEB_ANALYTICS: string | undefined;
+  readonly VITE_PRIVACY_COMPANY_NAME: string | undefined;
+  readonly VITE_PRIVACY_COMPANY_PARENTHESES_VALUE: string | undefined;
+  readonly VITE_PRIVACY_EMAIL: string | undefined;
+}
